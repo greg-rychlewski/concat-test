8985a7060bb302afd3e2477235c41534aacf0414
Greg Rychlewski <grychle@Gregs-MacBook-Pro.local>
2021-06-21 01:52:58 -0400

    reorganize

--- lib/concat_test.ex
+++ lib/concat_test.ex
@@ -5,19 +5,17 @@ end
 defimpl ConcatTest, for: List do
   def concat(enumerables) do
     if is_list(enumerables) and :lists.all(&is_list/1, enumerables) do
-      ConcatHelpers.concat_list(enumerables)
+      concat_list(enumerables)
     else
-      ConcatHelpers.concat_enum(enumerables)
+      concat_enum(enumerables)
     end
   end
-end
 
-defmodule ConcatHelpers do
-  def concat_list(enumerables) do
+  defp concat_list(enumerables) do
     :lists.append(enumerables)
   end
 
-  def concat_enum(enumerables) do
+  defp concat_enum(enumerables) do
     fun = &[&1 | &2]
     enumerables |> Enum.reduce([], &Enum.reduce(&1, &2, fun)) |> :lists.reverse()
   end
